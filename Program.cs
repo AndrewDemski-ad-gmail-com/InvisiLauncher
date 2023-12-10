d79b467893514cda9b26c143ebb2b3df6ade7854
AndrewDemski-ad-gmail-com <75534654+AndrewDemski-ad-gmail-com@users.noreply.github.com>
2023-12-10 19:20:27 +0100

    Working example

--- Program.cs
+++ Program.cs
@@ -1,34 +1,89 @@
+using System.Configuration;
 using System.Diagnostics;
+using System.Reflection;
+using System.Xml;
+using System.Xml.XPath;
 
 namespace InvisiLauncher
 {
-    internal static class Program
+    public static class Program
     {
         [STAThread]
-        static int Main(string[] args)
+        public static int Main(string[] args)
         {
-            if (args.Length == 1)
+            if (args is null || args.Length == 0)
             {
-                ProcessStartInfo psi = new ProcessStartInfo
-                {
-                    FileName = Environment.ExpandEnvironmentVariables(
-                        @"%Windir%\System32\WindowsPowerShell\v1.0\powershell.exe"),
-                    Arguments = "-NoLogo -Sta -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File \"" + args[0] + "\"",
-                    UseShellExecute = true,
-                    WindowStyle = ProcessWindowStyle.Hidden
-                };
-
-                Process process = new Process
-                {
-                    StartInfo = psi
-                };
-                process.Start();
-                process.WaitForExit();
-                return process.ExitCode;
+                Debug.WriteLine("args[] is null or empty! We need arguments in commandline, I cannot launch raw invisible PS console");
+                return -2;
             }
-            else
+            else // if (args.Length > 1)    //we expect only path to PS1 file, but accept everything else behind it
             {
-                return 666;
+                try
+                {
+                    string? filename_template;
+                    string? arguments_template;
+
+                    string ownPath = AppContext.BaseDirectory;
+                    XmlReaderSettings settings = new XmlReaderSettings();
+                    settings.IgnoreWhitespace = true;
+                    settings.IgnoreComments = true;
+                    using (var fileStream = File.Open("InvisiLauncher.config", FileMode.Open))
+                    {
+                        XPathDocument xPath = new XPathDocument(fileStream);
+                        XPathNavigator navigator = xPath.CreateNavigator();
+                        XmlNamespaceManager nameSpace = new XmlNamespaceManager(navigator.NameTable);
+                        nameSpace.AddNamespace("ns", "https://nonexistenthost.com/namespace");
+                        XPathExpression query;
+                        query = navigator.Compile("ns:configuration/ns:filename");
+                        query.SetContext(nameSpace);
+                        if (navigator.SelectSingleNode(query) is not null)
+                        {
+                            filename_template = navigator.SelectSingleNode(query).Value;
+                            Debug.WriteLine(string.Format("config value of {0} = {1}", nameof(filename_template), filename_template));
+                        }
+                        else
+                        {
+                            filename_template = @"%Windir%\System32\WINDOWSPOWERSHELL\v1.0\powershell.exe";
+                            Debug.WriteLine(string.Format("returning to default value of {0} = {1}", nameof(filename_template), filename_template));
+                        }
+                        query = navigator.Compile("ns:configuration/ns:args");
+                        query.SetContext(nameSpace);
+                        if (navigator.SelectSingleNode(query) is not null)
+                        {
+                            arguments_template = navigator.SelectSingleNode(query).Value;
+                            Debug.WriteLine(string.Format("config value of {0} = {1}", nameof(arguments_template), arguments_template));
+                        }
+                        else
+                        {
+                            arguments_template = @"-ExecutionPolicy Bypass -sta -noprofile -File {0}";
+                            Debug.WriteLine(string.Format("returning to default value of {0} = {1}", nameof(arguments_template), arguments_template));
+                        }
+                        Debug.WriteLine(string.Format("{0} Value = {1}", nameof(arguments_template), arguments_template));
+                    }
+                    string final_filename = Environment.ExpandEnvironmentVariables(filename_template);
+                    string final_arguments = string.Format(arguments_template, string.Join(" ", args));
+                    Debug.WriteLine(string.Format("{0} Value = {1}", nameof(final_filename), final_filename));
+                    Debug.WriteLine(string.Format("{0} Value = {1}", nameof(final_arguments), final_arguments));
+                    ProcessStartInfo info = new ProcessStartInfo(final_filename, final_arguments);
+                    info.UseShellExecute = false;
+                    info.RedirectStandardOutput = true;
+                    info.RedirectStandardError = true;
+                    info.RedirectStandardInput = false;
+                    info.WindowStyle = ProcessWindowStyle.Hidden;
+                    Process process = new()
+                    {
+                        StartInfo = info
+                    };
+                    process.Start();
+                    process.WaitForExit();
+                    int exitCode = process.ExitCode;
+                    return exitCode;
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                    return -3;
+                }
             }
         }
     }
